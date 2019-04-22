6a2099da03d3d9acc557064d72a6a13b5d1ca760
M. Zafer Yahsi <mzaferyahsi@gmail.com>
2019-04-22 21:48:36 +0200

    startup script updated

--- agent/start.sh
+++ agent/start.sh
@@ -1,1 +1,4 @@
-./wait-for ${LOGSTASH_HOST}:5001 -- ./wait-for ${KAFKA_HOST}:9092 -- pm2-runtime start pm2.json
+START_APP="pm2-runtime start pm2.json"
+WAIT_FOR_KAFKA="./wait-for.sh ${KAFKA_HOST}:9092 -- ${START_APP}"
+./wait-for.sh ${LOGSTASH_HOST}:5001 -- "$($WAIT_FOR_KAFKA)"
+
