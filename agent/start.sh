9a311898d22803b5e4348baa2d0e8d7651a250a1
M. Zafer Yahsi <mzaferyahsi@gmail.com>
2019-04-22 21:26:18 +0200

    Dockerfile updated

--- agent/start.sh
+++ agent/start.sh
@@ -1,1 +1,1 @@
-./app/node_modules/wait-for/wait-for ${KAFKA_HOST}:9092 -- ./app/node_modules/wait-for/wait-for ${LOGSTASH_HOST}:5001 -- pm2-runtime start pm2.json
+wait-for ${KAFKA_HOST}:9092 -- pm2-runtime start pm2.json
